59517938099875df5db2879db3fc9929b5d6b81f
Marek Ulwański <marek@ulwanski.pl>
2026-04-06 22:03:51 +0200

    Added migration with 'images' table

---
+++ migrations/1775502162425_database-init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE public.images (
+    id bigserial NOT NULL,
+    path character varying(255) NOT NULL,
+    title character varying(255),
+    width integer NOT NULL,
+    height integer NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.images OWNER to images;
+
+CREATE INDEX ON public.images USING btree (path) WITH (deduplicate_items=True);
+CREATE INDEX ON public.images USING btree (title text_pattern_ops) WITH (deduplicate_items=True);
+CREATE INDEX ON public.images USING btree (created_at) WITH (deduplicate_items=True);
